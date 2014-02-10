834a2c2449603316cf60ddfe85cdc366d654836c
Stefan Hedman <schteppe@gmail.com>
2014-02-10 09:25:27 +0100

    moved help page to main.c for portability

--- fmi-master/src/main.c
+++ fmi-master/src/main.c
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  * @author Adeel Asghar <adeel.asghar@liu.se>
  *
  * Created on: Oct 25, 2013
@@ -16,9 +16,75 @@ void printHeader() {
 }
 
 void printHelp() {
-  char command[200];
-  sprintf(command,"man %s",EXECUTABLE_NAME);
-  system(command);
+    printf("\n\
+FMI-MASTER\n\
+\n\
+    Server for distributed FMI simulation.\n\
+\n\
+FLAGS\n\
+\n\
+ -h Show help and quit.\n\
+ -l Show FMILibrary logs.\n\
+ -q Quiet mode, do not print simulation info to STDOUT. This flag should be used together with -r.\n\
+ -r Real time mode. Will usleep() in between communication steps if the execution was faster than real time.\n\
+ -v Show version and quit.\n\
+ -x Print XML and quit.\n\
+\n\
+OPTIONS\n\
+\n\
+ -c [CONNECTIONS]\n\
+    Connection specification. No connections by default. Quadruples of positive\n\
+    integers, representing which FMU and value reference to connect from and\n\
+    what to connect to. Syntax is\n\
+        CONN1:CONN2:CONN3...\n\
+    where CONNX is four comma-separated integers; FMUFROM,VRFROM,FMUTO,VRTO.\n\
+    An example connection string is\n\
+        0,0,1,0:0,1,1,1\n\
+    which means: connect FMU0 (value reference 0) to FMU1 (vr 0) and FMU0 (vr 1)\n\
+    to FMU1 (vr 1). Default is no connections.\n\
+ \n\
+ -d [TIMESTEP]  Timestep size. Default is 0.1.\n\
+ \n\
+ -f [OUTFORMAT] Output file format. Currently only \"csv\" is supported, and it\n\
+                is also the default.\n\
+ \n\
+ -g [STEPORDER] FMU stepping order. Used in serial algorithms. Should be a comma\n\
+                separated list of FMU indices. If the list starts with 0, then\n\
+                the first FMU will be stepped, and its utput values will be\n\
+                transferred through their connections (if any).\n\
+ \n\
+ -m [METHOD]    Stepping method. Available methods are \"jacobi\" (Jacobi,\n\
+                parallel) and \"gs\" (Gauss-Seidel, serial). Default is \"jacobi\".\n\
+ \n\
+ -o [OUTFILE]   Result output file. If set to \"stdout\", then output will be to\n\
+                STDOUT. Default value is \"result.csv\".\n\
+ \n\
+ -p [PARAMS]    Parameter specification. Triplets separated by :, specifying FMU\n\
+                index, value reference index and parameter value. Example\n\
+                setting the three first value references of FMU 0:\n\
+                \"0,0,12.3:0,1,true:0,2,9\". Default is no parameters.\n\
+ \n\
+ -s [SEPARATOR] CSV separator character. Default is comma (,).\n\
+ \n\
+ -t [ENDTIME]   End simulation time in seconds. Default is 1.0.\n\
+ \n\
+EXAMPLES\n\
+\n\
+    To run an FMU simulation from time 0 to 5 with timestep 0.01:\n\
+        fmi-master -t 5 -d 0.01 ../myFMU.fmu\n\n\
+\n\
+    To simulate two FMUs connected from the first output of the first FMU to the\n\
+    first input of the second:\n\n\
+        fmi-master -c 0,0,1,0 a.fmu b.fmu\n\
+\n\
+    To simulate quietly (without output to STDOUT) and save the results to file:\n\
+\n\
+        fmi-master -q -o out.csv a.fmu\n\
+\n\
+LICENSE\n\
+\
+\tMIT license\n\n");
+
 }
 
 int main( int argc, char *argv[] ) {
